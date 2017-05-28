acdae4454cd6b90bf011fb2b2fc333fc8b852fc3
Qingping Hou <dave2008713@gmail.com>
2017-05-28 00:39:31 -0700

    floydker(feat): show tag name in build command

--- floydker/src/floydker/build.py
+++ floydker/src/floydker/build.py
@@ -13,10 +13,17 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('dockerfile')
+@click.option('--show-tag-only/--no-show-tag-only',
+              help='skip build, only print out image tag name',
+              default=False)
 @click_log.simple_verbosity_option()
 @click_log.init(__name__)
-def build(dockerfile):
+def build(dockerfile, show_tag_only):
     image_tag = assert_image_tag_from_dockerfile(logger, dockerfile)
+    if show_tag_only:
+        print(image_tag)
+        return
+
     dockerfile_dir = os.path.dirname(dockerfile)
     logger.info('--------------------------------------------')
     logger.info('[*] Building %s with tag %s...', dockerfile, image_tag)
