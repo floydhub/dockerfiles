d3fb8701790236292eaf167c8874b43012292a2f
Qingping Hou <dave2008713@gmail.com>
2017-05-28 21:36:08 -0700

    ci: retry in build to workaround network failures

--- ci/build_images.sh
+++ ci/build_images.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+ANSI_RED="\033[31;1m"
+# shellcheck disable=SC2034
+ANSI_GREEN="\033[32;1m"
+ANSI_RESET="\033[0m"
+# shellcheck disable=SC2034
+ANSI_CLEAR="\033[0K"
+
+retry_cmd() {
+    local result=0
+    local count=1
+    set +e
+
+    retry_cnt=2
+    shift 1
+
+    while [ $count -le "${retry_cnt}" ]; do
+        [ $result -ne 0 ] && {
+            echo -e "\n${ANSI_RED}The command \"$*\" failed. Retrying, $count of ${retry_cnt}${ANSI_RESET}\n" >&2
+        }
+        "$@"
+        result=$?
+        [ $result -eq 0 ] && break
+        count=$((count + 1))
+        sleep 1
+    done
+
+    [ $count -gt "${retry_cnt}" ] && {
+        echo -e "\n${ANSI_RED}The command \"$*\" failed ${retry_cnt} times.${ANSI_RESET}\n" >&2
+    }
+
+    set -e
+    return $result
+}
+
 # set -o xtrace
 
 keepalive() {
@@ -23,7 +57,7 @@ else
     while read -r line; do
         echo "[*] Node ${CIRCLE_NODE_INDEX} running job ${line}..."
         dockerfile=$(cat "${line}")
-        floydker build "${dockerfile}" || {
+        retry_cmd floydker build "${dockerfile}" || {
             echo "Failed building ${dockerfile}."
             kill -9 "${ALIVEPID}"
             exit 1
